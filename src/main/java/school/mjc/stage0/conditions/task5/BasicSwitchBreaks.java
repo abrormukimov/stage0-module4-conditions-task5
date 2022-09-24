3ad517917e35b9007baca3b9523b8e09473fa2ba
abrormukimov <abrormm34@gmail.com>
2022-09-24 23:35:10 +0500

    Update tasks

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
@@ -4,12 +4,12 @@ public class BasicSwitchBreaks {
     public static void main(String[] args) {
         int a = 20;
         switch (a) {
+            case 20:
+                System.out.println("twenty");
             case 7:
                 System.out.println("seven");
             case 10:
                 System.out.println("ten");
-            case 20:
-                System.out.println("twenty");
             default:
                 System.out.println("default");
         }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,45 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month) {
+            case (1):
+                System.out.println("Winter");
+                break;
+            case (2):
+                System.out.println("Winter");
+                break;
+            case (3):
+                System.out.println("Spring");
+                break;
+            case (4):
+                System.out.println("Spring");
+            case (5):
+                System.out.println("Winter");
+                break;
+            case (6):
+                System.out.println("Summer");
+                break;
+            case (7):
+                System.out.println("Summer");
+                break;
+            case (8):
+                System.out.println("Winter");
+                break;
+            case (9):
+                System.out.println("Fall");
+                break;
+            case (10):
+                System.out.println("Fall");
+                break;
+            case (11):
+                System.out.println("Fall");
+                break;
+            case (12):
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("wrong number");
+                break;
+        }
     }
 }
